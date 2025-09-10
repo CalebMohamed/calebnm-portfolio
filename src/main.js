14ee0f0ef45b7e4303cf97eaea0183b8b022c05a
Caleb Mohamed <calebnoahmohamed@gmail.com>
2025-09-10 14:44:09 +0100

    putting the titlehop functionality in one place and importing + making footer go to bottom always

--- src/main.js
+++ src/main.js
@@ -1,51 +1,9 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
-import '../styles/components/header.css'
 import '../styles/components/generic_section.css'
 import '../styles/components/hero.css'
 import '../styles/components/display_cards.css'
+import '../styles/components/header.css'
 import '../styles/components/footer.css'
 import '../styles/utils.css'
-
-window.onload = function() {
-    convertHeadingsToSpans();
-    initialiseJumpUpRotations();
-}
-
-function convertHeadingsToSpans() {
-    let headings = document.querySelectorAll('h1, h2');
-    headings.forEach(heading => {
-        let headingText = heading.textContent;
-        heading.textContent = '';
-        headingText.split('').forEach(char => {
-            let newSpan = document.createElement('span');
-            // add the classes
-            newSpan.className += 'jump_up';
-            if (isVowel(char)){
-                newSpan.className += ' accented';
-            }
-            // adds in invisible i's for the spaces as the spaces are ignored
-            if (char == ' '){
-                newSpan.textContent = 'i';
-                newSpan.style.color = 'var(--clr-bg)';
-            } else {
-                newSpan.textContent = char;
-            }
-            // adds the span
-            heading.appendChild(newSpan);
-        });
-    });
-}
-
-function initialiseJumpUpRotations() {
-    let jump_ups = document.querySelectorAll('.jump_up');
-    jump_ups.forEach(span => {
-        var rotation = Math.random()*20 - 10;
-        span.style.setProperty('--random-rotation', rotation + 'deg');
-    });
-}
-
-function isVowel(char) {
-    char = char.toLowerCase();
-    return ("aeiou".indexOf(char) != -1);
-}
+import './titlehop.js'
